de423985c014826efe58ea45a8ed003effd34f4d
Haidy Nageh <haidy77749@gmail.com>
2025-10-10 21:24:42 +0200

    Home components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HelmetProvider } from "react-helmet-async";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./styles/main.css";
+import { store } from "./app/store";
+import AppRoutes from "./routes/AppRoutes";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const container = document.getElementById("root")!;
+const root = createRoot(container);
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 2,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+root.render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <AppRoutes />
+          </BrowserRouter>
+        </QueryClientProvider>
+      </Provider>
+    </HelmetProvider>
+  </React.StrictMode>
+);
+
+
